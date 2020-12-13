4ecdfb7f006945fa808318e5320eb0a2247d7468
sivayogasubramanian <sivayogasubramanian@gmail.com>
2020-12-13 18:40:02 +0800

    added initial user model with jwt auth

---
+++ app/lib/json_web_token.rb
@@ -0,0 +1,16 @@
+class JsonWebToken
+  SECRET = ENV["SECRET_KEY_BASE"] || Rails.application.secret_key_base
+  
+  def self.encode(payload, exp = 24.hours.from_now)
+    payload[:exp] = exp.to_i
+    JWT.encode(payload, SECRET)
+  end
+
+  def self.decode(token)
+    body = JWT.decode(token, SECRET)[0]
+    HashWithIndifferentAccess.new body
+  rescue
+    nil
+  end
+  
+end
