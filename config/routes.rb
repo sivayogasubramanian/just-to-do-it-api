216db2766af983b017953c3ac16e53ee98aa0d05
sivayogasubramanian <sivayogasubramanian@gmail.com>
2020-12-15 15:36:51 +0800

    improved code styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-
-  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post 'authenticate', to: 'authentication#authenticate'
-
+  resources :users
+  
   namespace :api do
     namespace :v1 do 
       resources :todos do
