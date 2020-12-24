1d3253ddf08da190c63e40da64ac3be392a208bc
sivayogasubramanian <sivayogasubramanian@gmail.com>
2020-12-24 22:11:27 +0800

    removed need to have a title for todos

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -2,7 +2,5 @@ class Todo < ApplicationRecord
   has_many :subtodos, dependent: :delete_all
   belongs_to :user
 
-  validates :title, presence: true
   validates :completed, inclusion: { in: [ true, false ] }
-  
 end
