647cd726c55bf590e03723d0776a627a79cebb62
sivayogasubramanian <sivayogasubramanian@gmail.com>
2020-12-25 17:33:32 +0800

    removed id from attributes

--- app/serializers/subtodo_serializer.rb
+++ app/serializers/subtodo_serializer.rb
@@ -1,4 +1,4 @@
 class SubtodoSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :title, :completed, :todo_id
+  attributes :title, :completed, :todo_id
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :name, :email
+  attributes :name, :email
 
 end
