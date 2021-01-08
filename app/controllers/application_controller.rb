08666217a5431a6ad80020e02c91e0b494d39bf6
sivayogasubramanian <sivayogasubramanian@gmail.com>
2021-01-08 15:51:53 +0800

    added default_startup_screen action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,20 @@
 class ApplicationController < ActionController::API
   before_action :authenticate_request
   attr_reader :current_user
+  skip_before_action :authenticate_request, :only => [:default_startup_screen]
+
+  def default_startup_screen
+    render html: '<h1>
+                    Just To-do it api is running
+                  </h1>
+                  <h3>
+                    Visit 
+                    <a href="https://just-to-do-it-website.netlify.app">
+                      https://just-to-do-it-website.netlify.app
+                    </a>
+                     to get started!
+                  </h3>'.html_safe
+  end
 
   private
 
