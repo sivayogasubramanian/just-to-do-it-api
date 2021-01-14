1593dd479b9995f6b5f1e7db84d32aa65a5782d7
sivayogasubramanian <sivayogasubramanian@gmail.com>
2021-01-14 21:46:41 +0800

    added before_create to set deleted bool on create

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,13 @@
 class Todo < ApplicationRecord
   has_many :subtodos, dependent: :delete_all
   belongs_to :user
+  before_create :set_deleted
 
   validates :completed, inclusion: { in: [ true, false ] }
+
+  private
+
+  def set_deleted
+    self.deleted = false;
+  end
 end
